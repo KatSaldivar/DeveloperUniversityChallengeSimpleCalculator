18e1f0b8c884eca22a51305b2a4dcf6cd4d8ec60
KatSaldivar <katherinekaufmann1@gmail.com>
2017-09-06 20:32:57 -0700

    Add project files.

---
+++ ChallengeSimpleCalculator/CalculatorWebForm.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CalculatorWebForm.aspx.cs" Inherits="ChallengeSimpleCalculator.CalculatorWebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Simple Calculator</h1>
+            <br />
+            First Value:&nbsp;
+            <asp:TextBox ID="firstValue" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Second Value:&nbsp;
+            <asp:TextBox ID="secondValue" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="additionButton" runat="server" Text="+" OnClick="additionButton_Click" />
+&nbsp;
+            <asp:Button ID="subtractButton" runat="server" Text="-" OnClick="subtractButton_Click" />
+&nbsp;
+            <asp:Button ID="multiplyButton" runat="server" Text="*" OnClick="multiplyButton_Click" />
+&nbsp;
+            <asp:Button ID="divideButton" runat="server" Text="/" OnClick="divideButton_Click" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server" BackColor="#00CCFF" Font-Bold="True" ForeColor="Black"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeSimpleCalculator/CalculatorWebForm.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class CalculatorWebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void additionButton_Click(object sender, EventArgs e)
+        {
+            float myFirstValue = float.Parse(firstValue.Text);
+            float mysecondValue = float.Parse(secondValue.Text);
+            float resultFloat = myFirstValue + mysecondValue;
+            string result = resultFloat.ToString();
+            resultLabel.Text = result;
+        }
+
+        protected void subtractButton_Click(object sender, EventArgs e)
+        {
+            float myFirstValue = float.Parse(firstValue.Text);
+            float mysecondValue = float.Parse(secondValue.Text);
+            float resultFloat = myFirstValue - mysecondValue;
+            string result = resultFloat.ToString();
+            resultLabel.Text = result;
+        }
+
+        protected void multiplyButton_Click(object sender, EventArgs e)
+        {
+            float myFirstValue = float.Parse(firstValue.Text);
+            float mysecondValue = float.Parse(secondValue.Text);
+            float resultFloat = myFirstValue * mysecondValue;
+            string result = resultFloat.ToString();
+            resultLabel.Text = result;
+        }
+
+        protected void divideButton_Click(object sender, EventArgs e)
+        {
+            float myFirstValue = float.Parse(firstValue.Text);
+            float mysecondValue = float.Parse(secondValue.Text);
+            float resultFloat = myFirstValue / mysecondValue;
+            string result = resultFloat.ToString();
+            resultLabel.Text = result;
+        }
+    }
+}
